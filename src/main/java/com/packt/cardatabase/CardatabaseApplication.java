e0339cab9435cb883d2cd6121d6e7857ae36d338
Obinna Uche <obinnauche01@outlook.com>
2025-09-11 19:05:42 +0100

    Added JWT Auth

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.packt.cardatabase.domain.AppUser;
+import com.packt.cardatabase.domain.AppUserRepository;
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.Owner;
@@ -19,10 +22,12 @@ public class CardatabaseApplication implements CommandLineRunner{
 			);
 	private final CarRepository repository;
 	private final OwnerRepository orepository;
+	private final AppUserRepository urepository;
 	
-	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository, AppUserRepository urepository) {
 		this.repository = repository;
 		this.orepository = orepository;
+		this.urepository = urepository;
 		
 	}
 	public static void main(String[] args) {
@@ -48,6 +53,13 @@ public class CardatabaseApplication implements CommandLineRunner{
 		for (Car car: repository.findAll()) {
 			logger.info("brand: {}, model: {}",car.getBrand(), car.getModel());
 		}
+		
+		//Username: user, password: user
+		urepository.save(new AppUser("user", "$2y$10$PVRIFvt/y2KdIS3q2kyJzupq2apxTPbj2HbFuRMfC8oBZjhS0CkkS", "USER"));
+		
+		//Username: admin, password: admin
+		urepository.save(new AppUser("admin", "$2y$10$2V1YkY9OqL85yEEYk5nih./fOr8D08GrIu/NGDhY2SyBP03YNM/TO", "ADMIN"));
+		
 		
 	}
 
---
+++ src/main/java/com/packt/cardatabase/service/JwtService.java
@@ -0,0 +1,54 @@
+package com.packt.cardatabase.service;
+
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import org.springframework.http.HttpHeaders;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+
+
+@Component
+public class JwtService {
+	static final long EXPIRATIONTIME = 8_640_000;
+	// 1 day in ms (Should be shorter in production )
+	
+	static final String PREFIX = "Bearer ";
+	// Generate secret key. Only for  development, in production i would read from application config
+	static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+	
+	//Generate signed jwt token
+	public String getToken(String username) {
+		String token = Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date (System.currentTimeMillis() + EXPIRATIONTIME))
+				.signWith(key)
+				.compact();
+		
+		return token;
+	}
+	// Get a token from request header Authorization header,
+	//verify the token, and get username
+	public String getAuthUser(HttpServletRequest request){
+		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+		
+		if (token != null) {
+			String user = Jwts.parserBuilder()
+					.setSigningKey(key)
+					.build()
+					.parseClaimsJws(token.replace(PREFIX, ""))
+					.getBody()
+					.getSubject();
+			
+			if (user != null)
+				return user;
+			
+			
+		}
+		return null;
+	}
+	
+}
